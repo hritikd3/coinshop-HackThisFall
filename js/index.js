25ad552a487e5fc246f99c3fad094834251deaac
Ashutosh Dash <dashashutosh1999@gmail.com>
2021-10-22 17:02:41 +0530

    terminal effect added

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "Cryptocurrencies",
+    "NFTs",
+    "and many more...",
+  ],
+  
+  smartBackspace: true, // Default value
+  typeSpeed: 50, 
+  backSpeed: 55, 
+  loop: true,
+  loopCount: Infinity,
+  startDelay: 0,
+  showCursor: false,
+})
